918c2a5448395766f3ba5362a0284cafc9069571
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ImmediatePostCellValues/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+using DevExpress.Xpf.Grid;
+
+namespace ImmediatePostCellValues {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            grid.ItemsSource = new ProductList();
+        }
+
+        private void TableView_CellValueChanging(object sender, DevExpress.Xpf.Grid.CellValueChangedEventArgs e) {
+            (sender as TableView).PostEditor();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to post values to a data source immediately after changing a cell value
+
+
+<p>To post a cell's value to a data source after it has been changed by an end-user, handle the <strong>CellValueChanging</strong> event and call the view's <strong>PostEditor</strong> method.</p>
+
+<br/>
+
+
